1b18e3351bcb234271109e6810e0d3f88427b39f
Lamoni Finlayson <lamonif@gmail.com>
2015-09-15 19:42:05 -0400

    Cleaned up some syntax

--- app/views.py
+++ app/views.py
@@ -1,38 +1,40 @@
 from flask import render_template, flash, redirect, jsonify
-from app import config
 from app import app
 from .forms import AddFlowForm
 import requests
-import pprint
 
 @app.route('/')
 @app.route('/index')
 def index():
 
-    r = requests.get('http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/',
-                     headers={
-                        'accept': 'application/json',
-                        'content-type': 'application/xml'
-                     },
-                     auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
-                     )
+    r = requests.get(
+            'http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/',
+            headers={
+                'accept': 'application/json',
+                'content-type': 'application/xml'
+             },
+             auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
+        )
 
-    return render_template('index.html',
-                           page_title='Dashboard',
-                           panel_title='Flows',
-                           flows_data=r.json()
-                           )
+    return render_template(
+        'index.html',
+        page_title='Dashboard',
+        panel_title='Flows',
+        flows_data=r.json()
+    )
 
 
 @app.route('/add_flow', methods=['GET'])
 def add_flow():
     form = AddFlowForm()
 
-    return render_template('add_flow.html',
-                           page_title='Add Flow',
-                           panel_title='Form',
-                           form=form
-                           )
+    return render_template(
+        'add_flow.html',
+        page_title='Add Flow',
+        panel_title='Form',
+        form=form
+    )
+
 
 
 @app.route('/api/add_flow', methods=['POST'])
@@ -66,33 +68,38 @@ def api_add_flow():
                     </instructions>
                 </flow>""" % (form.flow_priority.data, form.flow_destination_prefix.data, form.flow_id.data, form.flow_table_id.data)
 
-        r = requests.put('http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/node/openflow:1/table/%s/flow/%s' % (form.flow_table_id.data, form.flow_id.data),
-                         data=xml,
-                         headers = {
-                            'accept': 'application/json',
-                            'content-type': 'application/xml'
-                         },
-                         auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
-                         )
-
+        r = requests.put(
+                'http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/node/openflow:1/table/%s/flow/%s' % (form.flow_table_id.data, form.flow_id.data),
+                 data=xml,
+                 headers = {
+                    'accept': 'application/json',
+                    'content-type': 'application/xml'
+                 },
+                 auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
+             )
 
         if r.status_code == 200:
             return jsonify({'error': 0, 'message': 'Flow pushed!'})
 
         return jsonify({'error': 1, 'message': 'Unable to push flow'})
 
+
+
 @app.route('/remove_flow')
 def remove_flow():
 
-    r = requests.get('http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/',
-                     headers={
-                        'accept': 'application/json',
-                        'content-type': 'application/xml'
-                     },
-                     auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD']))
+    r = requests.get(
+        'http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/',
+         headers={
+            'accept': 'application/json',
+            'content-type': 'application/xml'
+         },
+         auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
+    )
 
-    return render_template('remove_flow.html',
-                           page_title='Remove Flow',
-                           panel_title='Form',
-                           flows_data=r.json()
-                           )
+    return render_template(
+        'remove_flow.html',
+        page_title='Remove Flow',
+        panel_title='Form',
+        flows_data=r.json()
+    )
