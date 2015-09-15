a04b5ec98c64fa93d4c8e5bb8551f234e8aeb501
Lamoni Finlayson <lamonif@gmail.com>
2015-09-15 19:31:54 -0400

    Added ajax form plugin, and also added ajax calls for add flow.  Still need to do remove flow

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, jsonify
 from app import config
 from app import app
 from .forms import AddFlowForm
@@ -8,21 +8,31 @@ import pprint
 @app.route('/')
 @app.route('/index')
 def index():
-    headers = {
-        'accept': 'application/json',
-        'content-type': 'application/xml'
-    }
 
-    r = requests.get('http://192.168.229.133:8181/restconf/config/opendaylight-inventory:nodes/', headers=headers, auth=('admin', 'admin'))
+    r = requests.get('http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/',
+                     headers={
+                        'accept': 'application/json',
+                        'content-type': 'application/xml'
+                     },
+                     auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
+                     )
 
-    return render_template('index.html', page_title='Dashboard', panel_title='Flows', flows_data=r.json())
+    return render_template('index.html',
+                           page_title='Dashboard',
+                           panel_title='Flows',
+                           flows_data=r.json()
+                           )
 
 
 @app.route('/add_flow', methods=['GET'])
 def add_flow():
     form = AddFlowForm()
 
-    return render_template('add_flow.html', page_title='Add Flow', panel_title='Form', form=form)
+    return render_template('add_flow.html',
+                           page_title='Add Flow',
+                           panel_title='Form',
+                           form=form
+                           )
 
 
 @app.route('/api/add_flow', methods=['POST'])
@@ -56,31 +66,33 @@ def api_add_flow():
                     </instructions>
                 </flow>""" % (form.flow_priority.data, form.flow_destination_prefix.data, form.flow_id.data, form.flow_table_id.data)
 
+        r = requests.put('http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/node/openflow:1/table/%s/flow/%s' % (form.flow_table_id.data, form.flow_id.data),
+                         data=xml,
+                         headers = {
+                            'accept': 'application/json',
+                            'content-type': 'application/xml'
+                         },
+                         auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD'])
+                         )
 
-        headers = {
-            'accept': 'application/json',
-            'content-type': 'application/xml'
-        }
 
-        r = requests.put('http://192.168.229.133:8181/restconf/config/opendaylight-inventory:nodes/node/openflow:1/table/%s/flow/%s' % (form.flow_table_id.data, form.flow_id.data), data=xml, headers=headers, auth=('admin', 'admin'))
+        if r.status_code == 200:
+            return jsonify({'error': 0, 'message': 'Flow pushed!'})
 
-
-
-        return r.text
+        return jsonify({'error': 1, 'message': 'Unable to push flow'})
 
 @app.route('/remove_flow')
 def remove_flow():
-    headers = {
-        'accept': 'application/json',
-        'content-type': 'application/xml'
-    }
 
-    r = requests.get('http://192.168.229.133:8181/restconf/config/opendaylight-inventory:nodes/', headers=headers, auth=('admin', 'admin'))
+    r = requests.get('http://'+app.config['ODL_SERVER_IP']+':'+app.config['ODL_SERVER_PORT']+'/restconf/config/opendaylight-inventory:nodes/',
+                     headers={
+                        'accept': 'application/json',
+                        'content-type': 'application/xml'
+                     },
+                     auth=(app.config['ODL_USERNAME'], app.config['ODL_PASSWORD']))
 
-    return render_template('remove_flow.html', page_title='Remove Flow', panel_title='Form', flows_data=r.json())
-
-
-@app.route('/read_config')
-def read_config():
-
-    return config.get_basic_auth()
+    return render_template('remove_flow.html',
+                           page_title='Remove Flow',
+                           panel_title='Form',
+                           flows_data=r.json()
+                           )
